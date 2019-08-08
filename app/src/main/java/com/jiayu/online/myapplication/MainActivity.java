4e9d238cc1f93ad0b9040718cfdd41068e7a7a87
wb <bing.wang@jiayuonline.com>
2019-08-08 14:59:34 +0800

    1.0.8 鉴权

--- app/src/main/java/com/jiayu/online/myapplication/MainActivity.java
+++ app/src/main/java/com/jiayu/online/myapplication/MainActivity.java
@@ -3,8 +3,14 @@ package com.jiayu.online.myapplication;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import com.wb.commonbase.manager.TaotutuManager;
+import com.wb.commonbase.util.GsonUtils;
+
+import java.util.HashMap;
 
 /***
  * commonbase 库项目
@@ -19,8 +25,19 @@ public class MainActivity extends AppCompatActivity {
         btn .setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i    = new Intent(MainActivity.this,TestActivity.class);
-                startActivity(i);
+//                Intent i    = new Intent(MainActivity.this,TestActivity.class);
+//                startActivity(i);
+
+                TaotutuManager.setAccessKey("7b51de35ba4f465a81c48af594df38e1");
+                TaotutuManager.setAccessSecret("56cbaf232dbd48d9b6b7b88760b5784e");
+
+
+                HashMap<String,String> map = new HashMap<>();
+                map.put("pageNo","1");
+                map.put("pageSize","10");
+
+                HashMap<String, String> param = TaotutuManager. getParam(map);
+                Log.d("test", GsonUtils.toJson(param));
             }
         });
     }
--- common_base/src/main/java/com/wb/commonbase/manager/TaotutuManager.java
+++ common_base/src/main/java/com/wb/commonbase/manager/TaotutuManager.java
@@ -6,11 +6,15 @@ import com.wb.commonbase.http.ExceptionHandler;
 import com.wb.commonbase.http.RetrofitClient;
 import com.wb.commonbase.util.SharedPreferenceUtil;
 
+import java.security.MessageDigest;
+import java.util.Arrays;
+import java.util.HashMap;
+
 public class TaotutuManager {
 
     static Context context;
-    static String token ;
-    static String uid;
+    static String accessSecret;
+    static String accessKey;
 
     public static void  init(Context c){
 
@@ -20,19 +24,88 @@ public class TaotutuManager {
         ExceptionHandler.init(context);
     }
 
-    public static String getToken() {
-        return token;
+
+    public void getParams(){
+
     }
 
-    public static void setToken(String token) {
-        TaotutuManager.token = token;
+    public static String getAccessSecret() {
+        return accessSecret;
     }
 
-    public static String getUid() {
-        return uid;
+    public static void setAccessSecret(String accessSecret) {
+        TaotutuManager.accessSecret = accessSecret;
     }
 
-    public static void setUid(String uid) {
-        TaotutuManager.uid = uid;
+    public static String getAccessKey() {
+        return accessKey;
+    }
+
+    public static void setAccessKey(String accessKey) {
+        TaotutuManager.accessKey = accessKey;
+    }
+
+    public static String MD5Encode(String origin, String charsetname) {
+        String resultString = null;
+        try {
+            resultString = new String(origin);
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            if (charsetname == null || "".equals(charsetname))
+                resultString = byteArrayToHexString(md.digest(resultString
+                        .getBytes()));
+            else
+                resultString = byteArrayToHexString(md.digest(resultString
+                        .getBytes(charsetname)));
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+        return resultString;
+    }
+
+    private static String byteArrayToHexString(byte b[]) {
+        StringBuffer resultSb = new StringBuffer();
+        for (int i = 0; i < b.length; i++)
+            resultSb.append(byteToHexString(b[i]));
+        return resultSb.toString();
+    }
+
+    private static String byteToHexString(byte b) {
+        int n = b;
+        if (n < 0)
+            n += 256;
+        int d1 = n / 16;
+        int d2 = n % 16;
+        return hexDigits[d1] + hexDigits[d2];
+    }
+
+    private static final String hexDigits[] = { "0", "1", "2", "3", "4", "5",
+            "6", "7", "8", "9", "a", "b", "c", "d", "e", "f" };
+
+    public static HashMap<String,String> getParam(HashMap<String,String> params){
+
+        String timestamp = String.valueOf(System.currentTimeMillis());
+
+//          accessSecret = "56cbaf232dbd48d9b6b7b88760b5784e";
+//          accessKey = "7b51de35ba4f465a81c48af594df38e1";
+
+        params.put("timestamp",timestamp);
+        params.put("accessSecret",accessSecret);
+
+        Object[] keys = params.keySet().toArray();
+        Arrays.sort(keys);
+
+        StringBuilder sb = new StringBuilder();
+        for(Object key:keys) {
+            sb.append(key + "=" + params.get(key) + "&");
+        }
+        sb.append("accessKey=" + accessKey);
+
+        String sign = MD5Encode(sb.toString(),"");
+        sign =  MD5Encode(sign,"");
+        sign =  MD5Encode(sign,"");
+
+        params.put("sign",sign);
+
+        return params;
     }
 }
